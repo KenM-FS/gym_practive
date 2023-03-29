a427eb97e17a0d3ac2a9cc6b58cd8f8b182da16e
Ken Matsuda <39686099+KenM-FS@users.noreply.github.com>
2023-03-29 18:36:36 +0900

    Add hello_baselines.py

--- gym_practice/hello_baselines.py
+++ gym_practice/hello_baselines.py
@@ -1,19 +1,24 @@
 import gymnasium as gym
+# from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines import PPO2
+from stable_baselines3.common.env_util import make_vec_env
+# from stable_baselines import PPO2
+from stable_baselines3 import PPO
 
-env = gym.make('CatPole-v1')
-env = DummyVecEnv([lambda: env]) #(1)
+# env = gym.make('CartPole-v1')
+env_id = 'CartPole-v1'
+# env = DummyVecEnv([lambda: env]) #(1)
+env = make_vec_env(env_id, n_envs=1, seed=0, vec_env_cls=DummyVecEnv)
 
 # Generate model (2)
-model = PPO2('MlpPolicy', env, verbose=1)
+model = PPO('MlpPolicy', env, verbose=1)
 
 # Learn model (3)
 model.learn(total_timesteps=100000)
 
 # Test model
 state = env.reset()
-for i in range(200):
+for i in range(2000):
   # render environment
   env.render()
 
